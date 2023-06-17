c66e1dba21ae638a43ed06e52e78f3870d80c8d5
Aashan Ghimire <ashanghimire10@gmail.com>
2023-06-17 19:54:33 +0545

    Added game enums

---
+++ src/Enums.cs
@@ -0,0 +1,17 @@
+namespace TicTacToe.Enums {
+    public enum Player {
+        X,
+        O
+    }
+
+    public enum GameMode {
+        SinglePlayer,
+        MultiPlayer
+    }
+
+    public enum Tile {
+        Empty,
+        X,
+        O
+    }
+};
