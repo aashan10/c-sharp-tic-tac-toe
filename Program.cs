df5e5f25cb8fd0549cd73a242ef5f7e2b2b0c944
Aashan Ghimire <ashanghimire10@gmail.com>
2023-06-17 21:16:49 +0545

    Modified program entry point

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Logic.Game();
+            game.Start();
+            
+        }
+    }
+}
